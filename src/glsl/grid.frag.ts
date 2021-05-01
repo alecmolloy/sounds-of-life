0b94f24778b198b2f94ab3ad0585dd96ab0788ad
Alec Molloy <alecmolloy@users.noreply.github.com>
2021-04-30 21:44:48 -0700

    more grid improvements

--- src/glsl/grid.frag.ts
+++ src/glsl/grid.frag.ts
@@ -15,7 +15,17 @@ const vec4 gridLineEmphasized = vec4(0.5, 0.5, 0.5, 1.0);
 const vec4 gridLine = vec4(0.2, 0.2, 0.2, 1.0);
 
 float gridShaping(float offsetComponent, float gridDivisor, float intensity) {
-    return clamp(-abs(-mod((offsetComponent + 0.5) * 2.0 + 1.0, (cellSize * gridDivisor) * 2.0) + 1.5) + 1.5, 0.0, intensity);
+    return
+        clamp(
+            -abs(
+                -mod(
+                    offsetComponent * devicePixelRatio * 2.0 + 1.0,
+                    (cellSize * gridDivisor) * devicePixelRatio * 2.0
+                ) + 1.5
+            ) + 1.5,
+            0.0,
+            intensity
+        );
 }
 
 void main() {
@@ -34,14 +44,17 @@ void main() {
         float y = gridShaping(screenCoordOffset.y, 1.0, 0.15);
         vec3 xy = vec3(max(x, y));
         
-        float x10 = gridShaping(screenCoordOffset.x, 10.0, 0.4);
-        float y10 = gridShaping(screenCoordOffset.y, 10.0, 0.4);
+        float x10 = gridShaping(screenCoordOffset.x, 10.0, 0.3);
+        float y10 = gridShaping(screenCoordOffset.y, 10.0, 0.3);
         vec3 xy10 = vec3(max(x10, y10));
         
         grid = max(xy, xy10);
     }
 
-    gl_FragColor = vec4(texture2D(state, (screenCoordOffset / stateSize) / cellSize).rgb + 
-        grid, 1.0);
-    }
+    gl_FragColor = vec4(texture2D(
+            state,
+            (screenCoordOffset / stateSize) / cellSize
+        ).rgb + grid, 1.0
+    );
+}
 `
